02f1ccb1576b97d9d54b7be0383b1496331c7b86
Vasyl Nesterenko <vasyl.nrk@gmail.com>
2016-12-24 20:18:56 -0800

    cange dir

---
+++ errortest.py
@@ -0,0 +1,64 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+
+regurl = "https://v2.whil.blue/sponsor/tml"
+#regurl = "https://connect.whil.com/sponsor/tml"
+
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_validform(driver):
+    driver.get(regurl)
+    driver.find_element_by_name("email").send_keys(Keys.TAB)
+    driver.find_element_by_name("first").send_keys(Keys.TAB)
+    driver.find_element_by_name("last").send_keys(Keys.TAB)
+    pw = "Passw0rd!"
+    driver.find_element_by_name("password").send_keys(Keys.TAB)
+    driver.find_element_by_name("passwordAgain").send_keys(Keys.TAB)
+
+    emaildanger = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='email']]//span[@class='text-danger' and text()!=0]").text
+    assert emaildanger == "An email is required", "Wrong email error text"
+
+    driver.find_element_by_name("email").send_keys("dsdfdfdsd", Keys.TAB)
+    withoutat = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='email']]//span[@class='text-danger' and text()!=0]").text
+    assert withoutat == "Invalid email. Missing '@' Missing '.'", "Wrong Assert @ Sign"
+
+    firstdanger = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='first']]//span[@class='text-danger' and text()!=0]").text
+    assert firstdanger == "Required", "Wrong First Name Error Message"
+
+    lastdanger = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='last']]//span[@class='text-danger' and text()!=0]").text
+    assert lastdanger == "Required", "Wrong Last Name Error Message"
+
+    emptyPass = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='password']]//span[@class='text-danger' and text()!=0]").text
+    assert emptyPass == "Required, at least 8 characters", "Wrong empty password message"
+
+    driver.find_element_by_name("password").send_keys("bla bla bla")
+    onlyletters = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='password']]//span[@class='text-danger' and text()!=0]").text
+    assert onlyletters == "Must have at least one number", "Wrong empty password message"
+
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("Blablabla43")
+    nopunctuation = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='password']]//span[@class='text-danger' and text()!=0]").text
+    assert nopunctuation == "Use at least one punctuation", "Wrong empty password message"
+
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("bla bla bla43")
+    nouppercase = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='password']]//span[@class='text-danger' and text()!=0]").text
+    assert nouppercase == "Use at least one uppercase character", "Wrong empty password message"
+
+
+    emptyPassAgain = driver.find_element_by_xpath("//div[contains(@class, 'form-group') and .//input[@name='passwordAgain']]//span[@class='text-danger' and text()!=0]").text
+    assert emptyPassAgain == "Required", "Wrong empty passwordAgain  message"
+
+    driver.find_element_by_xpath("//button/span[text()='JOIN']").click()
+
+    assert driver.current_url == regurl
--- registration.py
+++ registration.py
@@ -1,40 +1,52 @@
-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-regurl = "https://v2.whil.blue/sponsor/tml"
-mailurl = "http://email-fake.com/yopmail.pp.ua/"
+#@pytest.fixture
+# def driver(request):
+#     wd = webdriver.Chrome()
+#     request.addfinalizer(wd.quit)
+#     return wd
+from selenium.webdriver.support.wait import WebDriverWait
+
 maildriver = webdriver.Chrome()
 driver = webdriver.Chrome()
-maildriver.get(mailurl)
-email = maildriver.find_element_by_id("email_ch_text").text
-driver.get(regurl)
 
-driver.find_element_by_name("email").send_keys(email)
-driver.find_element_by_name("first").send_keys("Test")
-driver.find_element_by_name("last").send_keys("Test")
-select = Select(driver.find_element_by_name("requestPartyJoin.questionAnswers.1"))
-select.select_by_visible_text("London")
-select = Select(driver.find_element_by_name("requestPartyJoin.questionAnswers.2"))
-select.select_by_value("2,no")
-pw = "Passw0rd!"
-driver.find_element_by_name("password").send_keys(pw)
-driver.find_element_by_name("passwordAgain").send_keys(pw)
+def test_reg():
+    regurl = "https://v2.whil.blue/sponsor/tml"
+    mailurl = "http://email-fake.com/yopmail.pp.ua/"
+    maildriver = webdriver.Chrome()
+    driver = webdriver.Chrome()
+    maildriver.get(mailurl)
+    email = maildriver.find_element_by_id("email_ch_text").text
+    driver.get(regurl)
+    driver.find_element_by_name("email").send_keys(email)
+    driver.find_element_by_name("first").send_keys("Test")
+    driver.find_element_by_name("last").send_keys("Test")
+    select = Select(driver.find_element_by_name("requestPartyJoin.questionAnswers.1"))
+    select.select_by_visible_text("London")
+    select = Select(driver.find_element_by_name("requestPartyJoin.questionAnswers.2"))
+    select.select_by_value("2,no")
+    pw = "Passw0rd!"
+    driver.find_element_by_name("password").send_keys(pw)
+    driver.find_element_by_name("passwordAgain").send_keys(pw)
 
-
-def writemail():
+    driver.find_element_by_xpath("//button/span[text()='JOIN']").click()
+    # def writemail():
     file = open("mails.rtf", "a")
-    #file.read()
     file.writelines(email + '\n')
     file.close()
-writemail()
 
+    #wait = WebDriverWait(maildriver, 200)
+    #element = wait.until(EC.presence_of_element_located((By.LINK_TEXT,'Verify my email address')))
+    try:
+        WebDriverWait(maildriver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//a[text()="Verify my email address"]'))
+        )
+    finally:
+        print "bla bla"
+        maildriver.find_element_by_xpath('//a[text()="Verify my email address"]').click()
 
-
-
-
-
-
-driver.quit()
-maildriver.quit()
