c76a053fba2b264c97101e04e450169a79a21aa3
Vasyl-N <vasyl.nrk@gmail.com>
2016-12-27 10:20:34 -0800

    find_elements test

---
+++ find_elements.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("http://www.ebay.com/rpp/vehicles")
+
+links = driver.find_elements_by_xpath("//ul[@class='navigation-list nested']//a[@class='title extended-links']")
+print str(links)
+for i in links:
+    print i.text
+
+
+driver.quit()
