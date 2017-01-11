07083ae1791eddb4d0c1b2fb89f4af4a4a9206da
Vasyl-N <vasyl.nrk@gmail.com>
2017-01-10 16:40:48 -0800

    testMethods modified

--- sample.py
+++ sample.py
@@ -1,23 +1,8 @@
-import random
-from time import sleep
-
-from selenium import webdriver
-from selenium.webdriver import ActionChains
 
 import testMethods as t
-driver = webdriver.Chrome()
-email = "okumura81@yopmail.pp.ua"
-password = "Passw0rd!"
-# def test_ff():
-t.login(driver, email, password)
-sleep(1)
-driver.get("https://v2.whil.blue/onboarding/age")
-
-draggable_element = driver.find_element_by_css_selector("svg > circle:nth-of-type(2)")
-actions = ActionChains(driver)
-x = random.randint(-80, 80)
-y = random.randint(0, 170)
-actions.drag_and_drop_by_offset(draggable_element, x, y)
-actions.perform()
-
-t.selectMonth(driver)
+from selenium import webdriver
+def test_tip():
+    driver = webdriver.Chrome()
+    email = "okumura81@yopmail.pp.ua"
+    password = "Passw0rd!"
+    t.login(driver, email, password)
