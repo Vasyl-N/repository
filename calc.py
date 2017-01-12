4dba16e36da226850275d00a131ae35040bf28a7
vasyl-n <vasyl.nrk@gmail.com>
2017-01-12 08:09:15 -0800

    calc

---
+++ calc.py
@@ -0,0 +1,46 @@
+class Calculator(object):
+    def multdiv(self, s):
+        s = s.split()
+        i=0
+        while i < len(s):
+            if s[i] == "*":
+                res = float(s[i - 1]) * float(s[i + 1])
+                del s[i + 1], s[i], s[i - 1]
+                s.insert(i - 1, res)
+                i = 0
+            elif s[i] == "/":
+                res = round(float(s[i - 1]) / float(s[i + 1]), 2)
+                del s[i + 1], s[i], s[i - 1]
+                s.insert(i - 1, res)
+                i = 0
+            else: i += 1
+        return s
+
+
+    def sum(self, s):
+
+        i=0
+        while i <= len(s):
+            if s[i] == "+":
+                res = float(s[i - 1]) + float(s[i + 1])
+                del s[i + 1], s[i], s[i - 1]
+                s.insert(i - 1, res)
+                i = 0
+            elif s[i] == "-":
+                res = float(s[i - 1]) - float(s[i + 1])
+                del s[i + 1], s[i], s[i - 1]
+                s.insert(i - 1, res)
+                i = 0
+            elif len(s) == 1:
+                break
+            else:
+                i += 1
+        num = s[0]
+        if int(num)==num:
+            num = int(num)
+        print num
+        return num
+a = Calculator()
+def res(str):
+    Calculator.sum(a, Calculator.multdiv(a, str))
+res("23 - 5 + 5 * 6  *  2 / 2 + 50 / 2 * 6 + 3 / 3 * 2 * 10 / 13")
--- sample.py
+++ sample.py
@@ -1,23 +1,2 @@
-import random
-from time import sleep
-
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-
-import testMethods as t
-driver = webdriver.Chrome()
-email = "okumura81@yopmail.pp.ua"
-password = "Passw0rd!"
-# def test_ff():
-t.login(driver, email, password)
-sleep(1)
-driver.get("https://v2.whil.blue/onboarding/age")
-
-draggable_element = driver.find_element_by_css_selector("svg > circle:nth-of-type(2)")
-actions = ActionChains(driver)
-x = random.randint(-80, 80)
-y = random.randint(0, 170)
-actions.drag_and_drop_by_offset(draggable_element, x, y)
-actions.perform()
-
-t.selectMonth(driver)
+if 2 == 2.0:
+    print type(2.0)
